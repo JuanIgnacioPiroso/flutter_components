a46c4c955c509bacc7778cd21cf0b7a99607ba55
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-10 20:33:11 -0300

    initial flutter files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_components/screens/screens.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      home: const CardScreen(),
+      routes:{
+
+        'home':(context) => const HomeScreen(),
+        'listview2':(context) => const Listview1Screen(),
+        'alert':(context) => const AlertScreen(),
+        'card':(context) => const CardScreen()
+      }
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_components/screens/home_screen.dart';
+
+export 'package:flutter/material.dart';
+export 'package:flutter_components/screens/alert_screen.dart';
+export 'package:flutter_components/screens/card_screen.dart';
+export 'package:flutter_components/screens/listview1_screen.dart';
+export 'package:flutter_components/screens/home_screen.dart';
+
