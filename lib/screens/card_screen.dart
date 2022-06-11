6464034b61e3a9da89206ee29f5d8a80ec3078f4
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-10 23:14:07 -0300

    76

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -2,37 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_components/theme/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Card Widget'),
-      ),
-      body: ListView(
-
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-
-        children: [
-
-          Card(
-            child: Column(
-              children: const [
-
-                ListTile(
-
-                  title: Text('Soy un titulo'),
-                  leading: Icon(Icons.ac_unit_outlined, color: AppTheme.primary),
-                  subtitle:Text('Et deserunt excepteur aliqua fugiat sunt et qui nisi ut incididunt cillum.'),
-                )
-
-              ],
-            ),
-          )
-        ],
-      )
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                    title: Text('Soy un titulo'),
+                    leading:
+                        Icon(Icons.ac_unit_outlined, color: AppTheme.primary),
+                    subtitle: Text(
+                        'Et deserunt excepteur aliqua fugiat sunt et qui nisi ut incididunt cillum.'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
