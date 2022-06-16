140522f64c0daef2ff17aa0d66a6af5d8b1a73ad
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-16 01:50:51 -0300

    84

---
+++ lib/screens/avartar_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AvatarScreen extends StatelessWidget {
+   
+  const AvatarScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Juan Ignacio Piroso'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 5),
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Text('JP'),
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+         child: CircleAvatar(
+          maxRadius: 150,
+          backgroundImage: NetworkImage('https://i.postimg.cc/Bn99W7tV/flutter.jpg'),
+         )
+      ),
+    );
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,9 @@
 
+export 'package:flutter_components/screens/avartar_screen.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter_components/screens/alert_screen.dart';
 export 'package:flutter_components/screens/card_screen.dart';
 export 'package:flutter_components/screens/listview1_screen.dart';
 export 'package:flutter_components/screens/home_screen.dart';
+
 
