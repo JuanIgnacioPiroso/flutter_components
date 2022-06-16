e3103ff37080181392afe85c17d7fe39945de57b
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-06-16 01:40:19 -0300

    83

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,45 +1,66 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_components/theme/app_theme.dart';
 
 class AlertScreen extends StatelessWidget {
-
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context){
-
-    showDialog(
-      barrierDismissible: false,
-      context: context, 
-      builder: (context) {
-
-        return AlertDialog(
-
-          elevation: 5,
-          title: const Text('Titulo'),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Text('Este es el contenido de la alerta'),
-              SizedBox(height: 10),
-              FlutterLogo(size: 100),
-
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar')),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text(
+                    'Ok',
+                    style: TextStyle(color: Colors.blue),
+                  ))
             ],
-          ),
+          );
+        });
+  }
 
-          actions: [
-
-            TextButton(
-              onPressed: () => Navigator.pop(context), 
-              child: const Text('Cancelar')
-              )
-          ],
-        );
-      }
-      
-      );
-
-
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo'),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
   }
 
   @override
@@ -47,17 +68,19 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
         body: Center(
             child: ElevatedButton(
-          onPressed: () => displayDialog (context),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            child: Text('Mostrar alerta', style: TextStyle(fontSize: 20),),
-          )
-        )),
+                onPressed: () => Platform.isAndroid
+                    ? displayDialogAndroid(context)
+                    : displayDialogIOS(context),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Text(
+                    'Mostrar alerta',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ))),
         floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.close), 
-            
-            onPressed: () => Navigator.pop(context), 
-            )
-            );
+          child: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ));
   }
 }
